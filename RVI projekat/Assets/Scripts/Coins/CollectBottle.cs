228de1ba13ab94b18598881d0d01bfd054b29593
Vera Milosavljevic <vera.matf@gmail.com>
2022-05-26 14:36:19 +0200

    Small update

--- RVI projekat/Assets/Scripts/Coins/CollectBottle.cs
+++ RVI projekat/Assets/Scripts/Coins/CollectBottle.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class CollectBottle : MonoBehaviour
 {
     public AudioSource bottleFX;
+    public float heal = 5;
     private void OnTriggerEnter(Collider other) {
         bottleFX.Play();
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.SendMessage("HealDamage", Time.deltaTime * heal);
+        }
         this.gameObject.SetActive(false);
     }
 }
